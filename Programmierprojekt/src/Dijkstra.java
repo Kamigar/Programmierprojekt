cf8688458401f9d383503960c18d5bac194e9ab2
kamig <kamig@DESKTOP-EKK4S84>
2019-10-24 12:48:52 +0200

    f

--- Programmierprojekt/src/Dijkstra.java
+++ Programmierprojekt/src/Dijkstra.java
@@ -2,7 +2,7 @@
 public class Dijkstra {
 	
 	public static void main(String[] args) {
-		System.out.println("hello za warudo");
+		System.out.println("hello za warudoe");
 	}
 	public void dijkstra() {
 		System.out.println("if i see this on my laptop vi von ZULUL");
