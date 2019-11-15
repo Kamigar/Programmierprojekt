9ce08702f6ab232c2b9b85fc0507cfe32a5ee979
lyeb <luis.morgenstern@outlook.com>
2019-11-15 17:57:10 +0100

    Initial commit

---
+++ src/routeplanner/backend/app/Main.java
@@ -0,0 +1,9 @@
+package routeplanner.backend.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Anus");
+	}
+}
---
+++ src/routeplanner/backend/model/Edge.java
@@ -0,0 +1,29 @@
+package routeplanner.backend.model;
+
+public class Edge {
+
+	public Edge(int srcId, int trgId, double cost) {
+
+		_srcId = srcId;
+		_trgId = trgId;
+		_cost = cost;
+	}
+
+	public int srcId() {
+		return _srcId;
+	}
+
+	public int trgId() {
+		return _trgId;
+	}
+
+	public double cost() {
+		return _cost;
+	}
+
+
+	private int _srcId;
+	private int _trgId;
+
+	private double _cost;
+}
