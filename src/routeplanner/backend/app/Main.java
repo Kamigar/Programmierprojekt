44b0dc19c13dfd1c92a3d406188be380ca058cc5
lyeb <luis.morgenstern@outlook.com>
2019-11-17 19:22:02 +0100

    Added information about runtime of algorithm

--- src/routeplanner/backend/app/Main.java
+++ src/routeplanner/backend/app/Main.java
@@ -11,15 +11,28 @@ public class Main {
 
 		try {
 
-			//Node[] nodes = FileScanner.read("data/toy.fmi");
-			Node[] nodes = FileScanner.read("data/bw.fmi");
+			System.out.println("Reading file");
+			
+			long startTime = System.nanoTime();
+
+			Node[] nodes = FileScanner.read("data/toy.fmi");
+			//Node[] nodes = FileScanner.read("data/bw.fmi");
 			//Node[] nodes = FileScanner.read("data/stgtregbz.fmi");
 			
-			System.out.println("" + nodes.length + " nodes read");
+			long endTime = System.nanoTime();
+			
+			System.out.println("" + nodes.length + " nodes read in "
+					+ (double)(endTime - startTime) / 1000000000 + " seconds");
+			
+			startTime = System.nanoTime();
 			
 			Dijkstra.DijkstraStructure struct = Dijkstra.calculate(nodes, 3);
 			
+			endTime = System.nanoTime();
 			
+			System.out.println("Path calculated in "
+					+ (double)(endTime - startTime) / 1000000000 + " seconds");
+
 			int x = 34;
 
 		} catch (FileNotFoundException e) {
