a3e54facac67dfa42fa431e4e8e6311f3c971724
lyeb <luis.morgenstern@outlook.com>
2020-01-13 14:59:22 +0100

    Removed old build management and eclipse configuration

--- bin/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $0)
-
-javac -classpath . -d . $(find ../src -name "*.java")
-
-jar cmf manifest.mf routeplanner.jar routeplanner
-
--- bin/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $0)
-
-rm -rfv routeplanner routeplanner.jar
-
